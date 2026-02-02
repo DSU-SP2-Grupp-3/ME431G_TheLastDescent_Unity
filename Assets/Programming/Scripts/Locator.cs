c0ded75d23ef7239ecb74cb7bc5c33cd1793015e
Samuel Eskilson <samuel.eskilson@gmail.com>
2026-02-02 18:31:51 +0100

    service locator docs

--- Assets/Programming/Scripts/Locator.cs
+++ Assets/Programming/Scripts/Locator.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 
+/// <summary>
+/// A locator provides a reference to a component inheriting Service&lt;T>. For example: Locator&lt;Clock>.Get() returns
+/// a reference to the latest registered Clock instance or null if no Clock instance has registered itself or is active.
+/// </summary>
+/// <typeparam name="T">The type of the component inheriting Service&lt;T></typeparam>
 public class Locator<T> where T : Service<T>
 {
     private T service;
     
-    public Locator()
-    {
-        Locate();
-    }
-
     private void Locate()
     {
         service = Service<T>.instance as T;
@@ -16,11 +16,13 @@ public class Locator<T> where T : Service<T>
 
     public T Get()
     {
+        Locate();
         return service;
     }
 
     public bool TryGet(out T locatedService)
     {
+        Locate();
         if (service)
         {
             locatedService = service;
