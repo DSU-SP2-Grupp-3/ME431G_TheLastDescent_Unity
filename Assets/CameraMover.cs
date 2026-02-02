e81808ec2d467f5d83d7e2cfdfca711a5dffe702
Emily-Runesson <a24emiru@student.his.se>
2026-02-02 12:01:12 +0100

    Update CameraMover.cs

--- Assets/CameraMover.cs
+++ Assets/CameraMover.cs
@@ -1,12 +1,27 @@
+using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraMover : MonoBehaviour
 {
-    [Tooltip("")]
-    public Transform targetTransform;
+    [Header("Targets, written in order of priority")]
     
-    Update()
+    [Tooltip("The target Transform to move the camera from")]
+    public Transform targetGameObject;
+    [Tooltip("The point in space that the camera is focused on.")]
+    public Vector3 targetLocation;
+
+    private void Update()
     {
-        
+        /*needs to account for zoom/distance to object
+         which means vector math i bet :(*/
+        if (targetGameObject != null)
+        {
+            transform.position = targetGameObject.transform.position;
+        }
+        else
+        {
+            transform.position = targetLocation;
+        }
     }
 }
