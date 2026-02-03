a192511a26ae2390382c18f005e84bbc2936502f
Emily-Runesson <a24emiru@student.his.se>
2026-02-03 11:30:46 +0100

    Update CameraMover.cs

--- Assets/CameraMover.cs
+++ Assets/CameraMover.cs
@@ -5,7 +5,7 @@ using UnityEngine.UIElements;
 public class CameraMover : MonoBehaviour
 {
    
-    [Tooltip("The target Transform to move the camera from")]
+    [Tooltip("The target Transform to move the camera from, should be a model and not an actor if possible")]
     public Transform targetGameObject;
     [Tooltip("Zoom / Distance to target")]
     public float zoom;
