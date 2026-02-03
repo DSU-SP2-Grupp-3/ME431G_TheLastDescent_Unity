ce48f5555180bd4436517da51ad43061769671f2
Diddiking1 <svard_marcus@hotmail.com>
2026-02-03 15:04:23 +0100

    Mackan files removal

--- Assets/MackanTemp/Interaction.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class Interaction : ScriptableObject, IInteraction
-{
-    public virtual FloatRefrence runTime { get => runTime; set => runTime = value; }
-    public virtual void RunAction(float temp){}
-}
--- Assets/MackanTemp/ReadMe.txt
+++
@@ -1,2 +0,0 @@
-Lägger mina filer här för tillfället, vill försöka undvika merge conflicts så gott som möjligt.
-Tar bort efter riktig struktur har grundats i projektet.
