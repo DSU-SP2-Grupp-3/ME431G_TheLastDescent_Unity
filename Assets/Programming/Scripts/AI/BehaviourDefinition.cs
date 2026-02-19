4824c9e66d74ee99b9bfa4899e410bf2cfaec518
Samuel Eskilson <samuel.eskilson@gmail.com>
2026-02-19 10:30:55 +0100

    minor fixes

--- Assets/Programming/Scripts/AI/BehaviourDefinition.cs
+++ Assets/Programming/Scripts/AI/BehaviourDefinition.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
@@ -24,8 +23,8 @@ public abstract class BehaviourDefinition : ScriptableObject
             }
         }
         return shortest;
-    } 
-    
+    }
+
     protected NavMeshPath GetPathToNearestPosition(NavMeshAgent agent, List<Vector3> positions)
     {
         //this should return the path to the closest player. it just needs to be fed a navmesh agent and the player itself
@@ -68,7 +67,7 @@ public abstract class BehaviourDefinition : ScriptableObject
 
         return output;
     }
-    
+
     /// <summary>
     /// Trims the inputPath down based on the maximum distance provided
     /// </summary>
@@ -90,31 +89,38 @@ public abstract class BehaviourDefinition : ScriptableObject
             //this loop should go through the list and set the expensiveCorner int to the corner that it is too expensive to path to
             for (int i = 1; i < inputPath.corners.Length; i++)
             {
-                accumulatedDistance += Vector3.Distance(inputPath.corners[i-1],inputPath.corners[i]);
+                accumulatedDistance += Vector3.Distance(inputPath.corners[i - 1], inputPath.corners[i]);
                 if (accumulatedDistance > moveDistance)
                 {
-                    accumulatedDistance -= Vector3.Distance(inputPath.corners[i-1], inputPath.corners[i]) ;
+                    accumulatedDistance -= Vector3.Distance(inputPath.corners[i - 1], inputPath.corners[i]);
                     remainingDistance = agent.localStats.movement - accumulatedDistance;
                     expensiveCorner = i;
                     break;
                 }
             }
 
-            float distanceRatio = remainingDistance / Vector3.Distance(inputPath.corners[expensiveCorner - 1], inputPath.corners[expensiveCorner]);
+            float distanceRatio = remainingDistance / Vector3.Distance(inputPath.corners[expensiveCorner - 1],
+                inputPath.corners[expensiveCorner]);
             //x3 = x1 + t(x2-x1), t=d/D
-            Vector3 newDestination = 
+            Vector3 newDestination =
                 new Vector3(
-                    inputPath.corners[expensiveCorner - 1].x + (distanceRatio * (inputPath.corners[expensiveCorner].x - inputPath.corners[expensiveCorner - 1].x)),
-                    inputPath.corners[expensiveCorner - 1].y + (distanceRatio * (inputPath.corners[expensiveCorner].y - inputPath.corners[expensiveCorner - 1].y)),
-                    inputPath.corners[expensiveCorner - 1].z + (distanceRatio * (inputPath.corners[expensiveCorner].z - inputPath.corners[expensiveCorner - 1].z))
+                    inputPath.corners[expensiveCorner - 1].x + (distanceRatio *
+                                                                (inputPath.corners[expensiveCorner].x -
+                                                                 inputPath.corners[expensiveCorner - 1].x)),
+                    inputPath.corners[expensiveCorner - 1].y + (distanceRatio *
+                                                                (inputPath.corners[expensiveCorner].y -
+                                                                 inputPath.corners[expensiveCorner - 1].y)),
+                    inputPath.corners[expensiveCorner - 1].z + (distanceRatio *
+                                                                (inputPath.corners[expensiveCorner].z -
+                                                                 inputPath.corners[expensiveCorner - 1].z))
                 );
-            
+
             agent.navMeshAgent.CalculatePath(newDestination, inputPath);
-            
+
             return true;
         }
     }
-    
+
     // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/AI.NavMesh.SamplePosition.html
     protected bool RandomPoint(Vector3 center, float range, out Vector3 result)
     {
@@ -132,7 +138,7 @@ public abstract class BehaviourDefinition : ScriptableObject
         result = Vector3.zero;
         return false;
     }
-    
+
     public class BehaviourCommands
     {
         private List<Command> behaviourCommands;
