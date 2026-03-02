634cd52270b1e0ab8b17c93a2cf437cc031b06c4
Diddiking1 <svard_marcus@hotmail.com>
2026-03-02 14:58:14 +0100

    Added a settings menu

--- Assets/SettingsManager.cs
+++ Assets/SettingsManager.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class SettingsManager : Service<SettingsManager>
 {
     public UnityEvent open;
     public UnityEvent close;
+    //-Ma. Temp solution.
+    public Slider slider;
+        public Slider slider1;
+            public Slider slider2;
+
     public void Open()
     {
         open?.Invoke();
@@ -12,6 +18,22 @@ public class SettingsManager : Service<SettingsManager>
     public void Close()
     {
         close?.Invoke();
+    }
+    public void MasterVol()
+    {
+        AudioSettings.Instance.masterVolume = slider.value;
+    }    
+    public void SFXVol()
+    {
+        AudioSettings.Instance.effectVolume = slider1.value;
+    }
+    public void MusicVol()
+    {
+        AudioSettings.Instance.musicVolume = slider2.value;
+    }
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)) Open();
     }
 }
 
