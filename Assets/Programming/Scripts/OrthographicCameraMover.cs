f202613878c576c1e3927b99f3fbe5cd787fa29f
Samuel Eskilson <samuel.eskilson@gmail.com>
2026-03-02 12:19:34 +0100

    zoomies igen

--- Assets/Programming/Scripts/OrthographicCameraMover.cs
+++ Assets/Programming/Scripts/OrthographicCameraMover.cs
@@ -1,11 +1,22 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class OrthographicCameraMover : Service<OrthographicCameraMover>
 {
+    private Camera thisCamera;
+
     [Tooltip("The target Transform to move the camera from, should be a model and not an actor if possible")]
     public Transform targetGameObject;
-    [Tooltip("Zoom / Distance to target")]
-    public float zoom;
+
+    [SerializeField, Tooltip("Zoom grejer, min size är hur mycket man kan zoom ut, max är hur mycket man zoomar in")]
+    private float minSize, maxSize;
+
+    [SerializeField, Tooltip("size change per scroll tick")]
+    private float zoomSpeed;
+
+    private float targetZoomSize;
+    [SerializeField]
+    private float zoomSmoothing;
 
     [SerializeField]
     [Tooltip("Används i princip istället för att sätta positionen på kameran")]
@@ -21,6 +32,15 @@ public class OrthographicCameraMover : Service<OrthographicCameraMover>
     {
         Register();
         modeSwitcher = new();
+        thisCamera = GetComponent<Camera>();
+        targetZoomSize = thisCamera.orthographicSize;
+    }
+
+    private void Start()
+    {
+        InputManager im = new Locator<InputManager>().Get();
+        im.OnScrollUp += ZoomIn;
+        im.OnScrollDown += ZoomOut;
     }
 
     public void SetCameraTarget(Transform target)
@@ -40,5 +60,23 @@ public class OrthographicCameraMover : Service<OrthographicCameraMover>
         }
         Vector3 targetPosition = targetGameObject.position + offset;
         transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime * 100f);
+
+        thisCamera.orthographicSize = Mathf.Lerp(
+            thisCamera.orthographicSize,
+            targetZoomSize,
+            zoomSmoothing * Time.deltaTime * 100f
+        );
+    }
+
+    private void ZoomIn()
+    {
+        float newSize = targetZoomSize - zoomSpeed;
+        targetZoomSize = Mathf.Max(newSize, minSize);
+    }
+
+    private void ZoomOut()
+    {
+        float newSize = targetZoomSize + zoomSpeed;
+        targetZoomSize = Mathf.Min(newSize, maxSize);
     }
 }
