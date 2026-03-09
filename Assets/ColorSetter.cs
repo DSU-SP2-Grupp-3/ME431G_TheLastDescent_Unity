6a018ded02795ece5715621040e48610a396b107
Emily Blyndeff <a24emiru@student.his.se>
2026-03-09 13:06:33 +0100

    reworking color system, hopefully works better now (?)

--- Assets/ColorSetter.cs
+++ Assets/ColorSetter.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class ColorSetter : MonoBehaviour
 {
     [SerializeField] private SettingsStorage storedSettings;
-    [SerializeField] private RawImage targetImage;
+    [SerializeField] private RawImage myImage;
     private enum ColorType
     {
         HP, 
@@ -19,62 +19,35 @@ public class ColorSetter : MonoBehaviour
     
     [SerializeField] private ColorType colorType;
     private Color temporaryColor;
-    public void UpdateColor()
+    
+    public void SetColor() //updates the color stored in the settings
     {
         switch (colorType)
         {
+            //change stored color to color of button, send out event to all listeners
+            //-3 honestly a pretty bad way of doing it probably, but i would rather have it done poorly than not at all :<
             case ColorType.HP:
-                targetImage.color = storedSettings.PlayerHpColor;
+                storedSettings.PlayerHpColor = myImage.color;
                 break;
             case ColorType.AP:
-                targetImage.color = storedSettings.PlayerAPColor;
+                storedSettings.PlayerAPColor = myImage.color;
                 break;
             case ColorType.Heat:
-                targetImage.color = storedSettings.PlayerHeatColor;
+                storedSettings.PlayerHeatColor = myImage.color;
                 break;
             case ColorType.Path:
-                targetImage.color = storedSettings.PathColor;
+                storedSettings.PathColor = myImage.color;
                 break;
             case ColorType.Indicator:
-                targetImage.color = storedSettings.IndicatorColor;
+                storedSettings.IndicatorColor = myImage.color;
                 break;
+            
+            //these two dont need events as the created object always inherits the color correctly <3
             case ColorType.Damage:
-                targetImage.color = storedSettings.DamagePopColor;
+                storedSettings.DamagePopColor = myImage.color;
                 break;
             case ColorType.Heal:
-                targetImage.color = storedSettings.HealPopColor;
-                break;
-
-            default:
-                Debug.Log("HOW?? enum set to impossible option");
-                break;
-        }
-    }
-
-    public void SetColor()
-    {
-        switch (colorType)
-        {
-            case ColorType.HP:
-                storedSettings.PlayerHpColor = targetImage.color;
-                break;
-            case ColorType.AP:
-                storedSettings.PlayerAPColor = targetImage.color;
-                break;
-            case ColorType.Heat:
-                storedSettings.PlayerHeatColor = targetImage.color;
-                break;
-            case ColorType.Path:
-                storedSettings.PathColor = targetImage.color;
-                break;
-            case ColorType.Indicator:
-                storedSettings.IndicatorColor = targetImage.color;
-                break;
-            case ColorType.Damage:
-                storedSettings.DamagePopColor = targetImage.color;
-                break;
-            case ColorType.Heal:
-                storedSettings.HealPopColor = targetImage.color;
+                storedSettings.HealPopColor = myImage.color;
                 break;
 
             default:
--- Assets/Programming/Scripts/ScriptableObjects/SettingsStorage.cs
+++ Assets/Programming/Scripts/ScriptableObjects/SettingsStorage.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu(fileName = "SettingsStorage", menuName = "Scriptable Objects/SettingsStorage")]
 public class SettingsStorage : ScriptableObject
 {
+    
     //-E this is literally just used to store variables across scenes so that settings changed in the menu will stay changed in the game <3
     [Header("Sound/FMOD")] public float masterVolume;
     public float effectVolume;
@@ -10,13 +12,46 @@ public class SettingsStorage : ScriptableObject
     public float ambienceVolume;
     public float dialogueVolume;
 
-    [Header("Color")] public Color PlayerHpColor;
+    [Header("Color")] 
+    public Color PlayerHpColor;
     public Color PlayerAPColor;
     public Color PlayerHeatColor;
 
     public Color PathColor;
     public Color IndicatorColor;
 
-    public Color DamagePopColor;
-    public Color HealPopColor;
+    public Color DamageColor;
+    public Color HealColor;
+    
+    //-E i have to make events :(
+    public void TriggerAll() //just triggers all the events <3
+    {
+        SetPlayerHpColor(PlayerHpColor);
+        SetPlayerAPColor(PlayerAPColor);
+        SetPlayerHeatColor(PlayerHeatColor);
+        SetPathColor(PathColor);
+        SetIndicatorColor(IndicatorColor);
+        SetDamagePopColor(DamageColor);
+        SetHealPopColor(HealColor);
+    }
+    //player HP
+    public UnityAction<Color> PlayerHpColorEvent = delegate{}; public void SetPlayerHpColor(Color input) { PlayerHpColorEvent(input); }
+    
+    //player AP
+    public UnityAction<Color> PlayerAPColorEvent = delegate{}; public void SetPlayerAPColor(Color input) { PlayerAPColorEvent(input); }
+    
+    //player Heat ;) <- silly
+    public UnityAction<Color> PlayerHeatColorEvent = delegate{}; public void SetPlayerHeatColor(Color input) { PlayerHeatColorEvent(input); }
+    
+    //path color
+    public UnityAction<Color> PathColorEvent = delegate{}; public void SetPathColor(Color input) { PathColorEvent(input); }
+    
+    //indicator color (no idea how this one will work tbh
+    public UnityAction<Color> IndicatorColorEvent = delegate{}; public void SetIndicatorColor(Color input) { IndicatorColorEvent(input); }
+    
+    //damage pop, might not get used but it now exists at least?
+    public UnityAction<Color> DamagePopColorEvent = delegate{}; public void SetDamagePopColor(Color input) { DamagePopColorEvent(input); }
+    
+    //heal pop, might not get used but it now exists at least?
+    public UnityAction<Color> HealPopColorEvent = delegate{}; public void SetHealPopColor(Color input) { HealPopColorEvent(input); }
 }
