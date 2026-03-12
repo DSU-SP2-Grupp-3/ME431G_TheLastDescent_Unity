5a4ead4d3463699948af04cde2d7c2c5e3a40965
Samuel Eskilson <samuel.eskilson@gmail.com>
2026-03-12 11:10:44 +0100

    stop roundclock on dialogo

--- Assets/Programming/Scripts/Commands/DialogueCommand.cs
+++ Assets/Programming/Scripts/Commands/DialogueCommand.cs
@@ -11,29 +11,39 @@ public class DialogueCommand : Command
     public DialogueCommand(DialogueScriptable dialogueScriptable, WorldAgent invokingAgent) : base(invokingAgent)
     {
         this.dialogueScriptable = dialogueScriptable;
-
     }
     protected override IEnumerator Execute()
     {
         inputManager = new Locator<InputManager>().Get();
         Button turnBasedButton = new Locator<ToggleTurnBasedButton>().Get().GetComponent<Button>();
+        RoundClock roundClock = new Locator<RoundClock>().Get();
+        bool dialoguePausedRoundClock = false;
+
         dialogueServiceLocator = new();
+
         if (dialogueServiceLocator.TryGet(out dialogueService))
         {
             if (turnBasedButton) turnBasedButton.interactable = false;
+            if (roundClock.currentMode == RoundClock.ProgressMode.RealTime)
+            {
+                dialoguePausedRoundClock = true;
+                roundClock.EnterTurnBased();
+            }
             inputManager.enabled = false;
-            yield return dialogueService.StartCoroutine(dialogueService.InitializeDialouge(dialogueScriptable.GetDialogues()));
+            yield return dialogueService.StartCoroutine(
+                dialogueService.InitializeDialouge(dialogueScriptable.GetDialogues()));
             Debug.Log("Done");
             inputManager.enabled = true;
             if (turnBasedButton) turnBasedButton.interactable = true;
-        }
 
+            if (dialoguePausedRoundClock)
+            {
+                roundClock.EnterRealTime();
+            }
+        }
     }
     public override void Break() { }
     public override float apCost { get; }
     /// <inheritdoc />
     public override float resourceCost => 0f;
 }
-
-
-
