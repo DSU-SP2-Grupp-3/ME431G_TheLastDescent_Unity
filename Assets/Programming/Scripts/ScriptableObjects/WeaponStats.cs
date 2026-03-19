621e70345cd8e3a2ec1bcac72b7190ec21d7dc54
Samuel Eskilson <samuel.eskilson@gmail.com>
2026-03-19 17:34:24 +0100

    moved weapon sfx to weapon stats

--- Assets/Programming/Scripts/ScriptableObjects/WeaponStats.cs
+++ Assets/Programming/Scripts/ScriptableObjects/WeaponStats.cs
@@ -5,8 +5,11 @@ public class WeaponStats : ScriptableObject
 {
     [SerializeField]
     private float minDamage, maxDamage, range, cost = 1f;
+    [SerializeField]
+    private EventScriptable attackSfxEvent;
 
     public float attackRange => range;
     public float attackCost => cost;
     public float GetDamage() => Random.Range(minDamage, maxDamage);
+    public EventScriptable attackEvent => attackSfxEvent;
 }
