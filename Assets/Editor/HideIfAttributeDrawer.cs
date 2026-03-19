78fc20c5b9c44d056100cf224ccdee50cef8b13f
Samuel Eskilson <samuel.eskilson@gmail.com>
2026-03-19 14:18:04 +0100

    hide if attribute

---
+++ Assets/Editor/HideIfAttributeDrawer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Object = UnityEngine.Object;
+
+[CustomPropertyDrawer(typeof(HideIfAttribute))]
+public class HideIfAttributeDrawer : PropertyDrawer
+{
+    private bool Hidden(SerializedProperty property)
+    {
+        return Hidden(property.serializedObject);
+    }
+
+    private bool Hidden(SerializedObject serializedObject)
+    {
+        HideIfAttribute hideIf = attribute as HideIfAttribute;
+
+        MethodInfo method = serializedObject.targetObject.GetType().GetMethod(
+            hideIf.predicateName
+        );
+
+        Object instance = serializedObject.targetObject as Object;
+
+
+        bool hide = false;
+
+        if (method != null && method.ReturnType == typeof(bool))
+        {
+            hide = (bool)method.Invoke(instance, Array.Empty<object>());
+        }
+
+        return hide;
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        if (!Hidden(property))
+        {
+            EditorGUI.PropertyField(position, property, label);
+        }
+    }
+
+    public override VisualElement CreatePropertyGUI(SerializedProperty property)
+    {
+        VisualElement container = new VisualElement();
+        
+        container.Add(new PropertyField(property));
+        
+        return container;
+        
+        // only works for IMGUI right now, below code must be fixed so property is redrawn when the object changes
+        
+        // if (!Hidden(property))
+        // {
+        //     container.Add(new PropertyField(property));
+        // }
+        //
+        //
+        // // container.Bind(property.serializedObject);
+        // container.TrackSerializedObjectValue(property.serializedObject, so =>
+        // {
+        //     Debug.Log("track");
+        //     container.Clear();
+        //     if (!Hidden(so))
+        //     {
+        //         container.Add(new PropertyField(so.FindProperty(property.propertyPath)));
+        //     }
+        //     container.MarkDirtyRepaint();
+        // });
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return Hidden(property) ? 0f : base.GetPropertyHeight(property, label);
+    }
+}
---
+++ Assets/Programming/Attributes/HideIfAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Field)]
+public class HideIfAttribute : PropertyAttribute
+{
+    public string predicateName;
+    
+
+    /// <summary>
+    /// The name of a method on this object that returns a bool, and takes no parameters
+    /// The method should return true if this field should be hidden in the inspector, otherwise false
+    /// </summary>
+    public HideIfAttribute(string predicateName)
+    {
+        this.predicateName = predicateName;
+    }
+}
