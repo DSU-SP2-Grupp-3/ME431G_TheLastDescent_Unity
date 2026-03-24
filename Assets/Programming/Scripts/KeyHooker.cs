02938b601eb20fdbec6983877ba01d0814f7cc2f
Samuel Eskilson <samuel.eskilson@gmail.com>
2026-03-24 14:04:23 +0100

    simple credits scene

--- Assets/Programming/Scripts/KeyHooker.cs
+++ Assets/Programming/Scripts/KeyHooker.cs
@@ -1,16 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class KeyHooker : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private KeyCodeHook[] hooks;
+
+    private void OnGUI()
     {
-        
+        if (Event.current.type == EventType.KeyDown && Event.current.keyCode != KeyCode.None)
+        {
+            OnKeyPressed(Event.current.keyCode);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnKeyPressed(KeyCode keyCode)
     {
-        
+        IEnumerable<KeyCodeHook> matching = hooks.Where(h => h.keyCode == keyCode);
+
+        foreach (KeyCodeHook hook in matching)
+        {
+            hook.OnKeyPressed?.Invoke();
+        }
+    }
+
+    [Serializable]
+    private class KeyCodeHook
+    {
+        public KeyCode keyCode;
+        public UnityEvent OnKeyPressed;
     }
 }
